cdb6242d13da58b86c7a7b8d40a04633a1189973
Liron Tzemach <lirontzemach@gmail.com>
2020-05-14 19:57:20 +0300

    without gui

--- Apps/tic_tac_toe_graphics/Source/MainComponent.h
+++ Apps/tic_tac_toe_graphics/Source/MainComponent.h
@@ -5,26 +5,16 @@
 #include "game_manager.h"
 #include "BoardGui.h"
 
-class GuiController : public Liron486::Gui
+class GuiController
 {
 public:
     GuiController(Liron486::GameManager& managerToUse, BoardGui& boardToUse)
-        : Gui(managerToUse), board(boardToUse)
+        :board(boardToUse)
     {
     }
 
 private:
-    void printBoard() override { board.updateFrom(gameData.getGameData().board);}
-    void printHeader() override {}
-    void printHeaderWithoutDiff() override {}
-    void weHaveAWinner(int winerIndex) override {}
-    void tie() override {}
-    void tutorial() override {}
-    Liron486::Point makeMove(int playerIndexToUse) const override
-    {
-        return Liron486::Point();
-    }
-    ActionEnum wantToPlayAgain() const override { return ActionEnum::No; }
+
 
     BoardGui& board;
 };
